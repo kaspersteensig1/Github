c99916bb0df76a0ceb657667593bc3ed41a7ed9d
Kasper <KasperSteensig@Kaspers-MacBook-Air.local>
2016-05-17 00:34:38 +0200

    indhold
    Siderne har fået en del indhold og er lidt mere spæende nu..

--- kontakt/formmail.php
+++ kontakt/formmail.php
@@ -1,22 +1,23 @@
 <?php
 
-// Surftown formmail
-
 // angiv modtagere af formmailen
 // flere modtagere kan tilføjes som
 // $modtagere[1] = "adresse@domain.dk"
 // $modtagere[2] = "adresse2@domain.dk"
 // osv.
-$modtagere[0] = "exempel@exempel";
+
+$modtagere[0] = "steensig91@gmail.com";
 //$modtagere[1] = "";
+//$modtagere[2] = "";
 
 // succes og fejlsider
 $succes = "succes.html";
 $fejl = "fejl.html";
 
-// standard tesktbokse er
+// tesktbokse er
 // navn
 // emailadresse
+// telefonnummer
 // emne
 // besked
 
@@ -28,13 +29,17 @@ $mail_modtagere = implode(",", $modtagere);
 
 // klargør parametre
 $navn = $_POST['navn'];
-$emailadresse = "From: " . $_POST['emailadresse'];
+$emailadresse = "fra: " . $_POST['emailadresse'];
 $emne = "Besked fra " . $navn . ": " . $_POST['emne'];
 $besked = $_POST['besked'];
+$telefon = $_POST['telefon'];
+
 
 // send mail
 $mail_status = mail($mail_modtagere, $emne, $besked, $emailadresse);
 
+
+//vælger hvilken side du blir sendt til efter sendt eller ikke sendt mail.
 if ($mail_status) {
 header("Location: " . $succes);
 } else {
